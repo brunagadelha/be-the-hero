471e4d101c10ee270a85d093735dc74bf3b2313e
Bruna Gadelha <bruna_gadelha@live.com>
2020-03-29 16:47:17 -0300

    Front-end da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,7 @@ const routes = require("./routes");
 
 const app = express();
 
-app.use(
-  cors({
-    //origin: 'http:meuapp.com
-  })
-);
-
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.listen(3333);
